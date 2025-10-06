0be191b911c50c3679b659eade41dfdc8ae4168e
MalikAliyan-Alam <aliyanusama557@gmail.com>
2025-10-06 11:59:29 -0700

    assignment

---
+++ app.js
@@ -0,0 +1,44 @@
+console.log("Javascript Connected!");
+// var firstName = prompt("Enter Your First Name" , "Malik");
+// var lastName = prompt("Enter Your Last Name" , "Aliyan");
+
+// alert("WellCome! " + firstName + " " + lastName);
+
+// Assignment 21 part 1 completed
+
+// var mobileModel = prompt("Enter The Name of your Favorite Mobile Model!" , "Samsung S24 Ultra");
+
+// var inputLenght = mobileModel.length;
+// console.log(inputLenght);
+
+// alert("Your Favorite phone is : " + mobileModel + " and the string lenght is " + inputLenght);
+
+// Assignment 21 part 2 completed
+
+// var country = "pakistan";
+// var codeAt = country.indexOf("n");
+// console.log(codeAt);
+// document.write("String : " + country + "<br/>");
+// document.write("Index of 'n' is : " + codeAt);
+
+
+
+// Assignment 21 part 3 completed
+
+// var greeting = "Hello World!";
+// var lastIndex = greeting.lastIndexOf('l');
+// document.write("String : " + greeting + "<br/>");
+// document.write("The last l is at index  : " + lastIndex);
+// Assignment 21 part 4 completed
+
+// var greeting = "Pakistani!";
+// var lastIndex = greeting.IndexOf('i');
+// document.write("String : " + greeting + "<br/>");
+// document.write("The  l is at index  : " + lastIndex);
+
+// Assignment 21 part 5 completed
+
+
+
+// Assignment 21 part 5 completed
+
